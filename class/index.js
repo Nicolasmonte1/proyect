886727c31d77f7bc029db111a19639252952a1c7
NicolasMonte <nicolas.montecchiani@hotmail.com>
2023-02-18 20:28:07 -0300

    1

---
+++ class/index.js
@@ -0,0 +1,32 @@
+class ProductManager {
+    title = ""
+    description = ""
+    price = " "
+    thumbnail = ""
+    code = ""
+    stock = ""
+
+    constructor (title, description, price, thumbnail, code, stock) {
+        products = [
+        this.title = title,
+        this.description = description,
+        this.price = price,
+        this.thumbnail = thumbnail,
+        this.code = code,
+        this.stock = stock,
+    ]
+    }
+}
+
+module.exports = ProductManager
+
+
+addProduct() {
+    products.push()
+}
+
+
+getElements() {
+    return products ()
+}
+
