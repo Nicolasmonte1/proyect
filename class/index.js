dbd0b8ea59ce56308bcc64c6daf7d245a8f8ddc0
NicolasMonte <nicolas.montecchiani@hotmail.com>
2023-03-01 23:28:36 -0300

    Upload changes 01-03-23

--- class/index.js
+++ class/index.js
@@ -1,43 +1,27 @@
 class ProductManager {
-    title = ""
-    description = ""
-    price = " "
-    thumbnail = ""
-    code = ""
-    stock = ""
-    id = ""
 
 constructor() {
-        this.products = []
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+        this.id = id.products.lenght + 1;
+        this.products = [];
+        this.path;
       }
 
-addProduct (title, description, price, thumbnail, code, stock, id) {
-        this.title = title
-        this.description = description
-        this.price = price
-        this.thumbnail = thumbnail
-        this.code = code
-        this.stock = stock
-        this.id = id.products.id.lenght + 1
-    }
-
-addProduct() {
-    products.push()
+addProduct(product) {
+    this.products.push(product);
 }
-
 
 getElements() {
-    return products ()
+    return this.products;
 }
 
-getElementsbyId(getid) {
-    const getid = this.id
-
-    if(getid === false){
-        return "Producto no encontrado"
-    }
-
-    return getid
+getElementsbyId(getId) {
+    return this.products.find(product => product.id === getId);
 }
 
 }
